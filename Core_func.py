52800d22953b6a1f742c18f18e9bb0246b12a9a2
WuChou2012 <wcraig2012@outlook.com>
2018-05-24 12:07:38 +0800

    20180524_wuchou_1
    ###Core_func.py
    1.change function name
    2.change return value with (state, content)
    3.put three ways in creating accounts by Generate_Three_Key() function
    4.change function Import_Keystore() filename -> filecontent which is jsonable
    5.add test cases

--- Core_func.py
+++ Core_func.py
@@ -1,42 +1,59 @@
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-import sys
 import getpass
-
-from web3.auto import w3
+import json
+import sys
 
 from eth_account import Account
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from web3.auto import w3
 
 
+"""
+failure reason for Generate_Two_Key function
+10000: password length less than 6
+10001: two passwords do not match
+"""
 
-def Generate_Key(password1, password2):
+
+def Generate_Three_Key(password1, password2):
     if password1 == password2:
         pass1 = password1
         pass2 = password2
         if len(pass1) < 6:
             print('less than 6')
-            return 1
+            return (0, 10000)
         else:
             acct = Account.create(password1)
-            return acct
+            public_key = acct.address
+            private_key = acct.privateKey
+            encrypted = Account.encrypt(private_key, password1)
+            return (1, [public_key, w3.toHex(private_key), json.dumps(encrypted)])
     else:
         print('wrong')
-        return 1
-
-# wuc
-def Import_secret(passphrase1, passphrase2, secret):
-    if passphrase1 == passphrase2:
-        print(passphrase1, secret)
-    else:
-        print('wrong')
+        return (0, 10001)
 
 
-def Import_Ketstore(passphrase, filename):
-    if filename.len == 0:
-        print('no file')
-    else:
-        print('keystore', passphrase)
+"""
+failure reason for Generate_Key function
+"""
+
+
+def Import_From_Private(secret):
+    acct = Account.privateKeyToAccount(secret)
+    public_key = acct.address
+    return (1, public_key)
+
+
+def Import_Keystore(passphrase, filecontent):
+    try:
+        # content = json.loads(filecontent)
+        private_key = w3.toHex(Account.decrypt(filecontent, passphrase))
+        public_key = Account.privateKeyToAccount(private_key).address
+        return (1, [public_key, private_key])
+    except Exception as err:
+        print(err)
+        return (0, 10000)
 
 
 def Import_mnemonic(passphrase1, passphrase2, mnemonicwords):
@@ -46,6 +63,17 @@ def Import_mnemonic(passphrase1, passphrase2, mnemonicwords):
     else:
         print('wrong')
 
-def Transaction_out(fromaddr,toaddr,value,gas,gasprice):
+
+def Transaction_out(fromaddr, toaddr, value, gas, gasprice):
     print(' ')
 
+
+# test for Generate_Two_Key()
+# print(Generate_Two_Key("12345", "12345"))
+# print(Generate_Two_Key("1234567", "123456"))
+test_account = (Generate_Three_Key("123456", "123456"))
+print(test_account)
+test_keystore = test_account[1][2]
+
+# test for Import_Keystore
+print(Import_Keystore("123456", test_keystore))
